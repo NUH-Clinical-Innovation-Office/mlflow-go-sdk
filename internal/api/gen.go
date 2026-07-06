9947858b297b4cf18f37bb7a031dded8a6dcaf09
Yong Cheng Low <yongchenglow@Yongs-MacBook-Air.local>
2026-07-06 16:45:00 +0800

    chore: strip backend template scaffolding to clean library module

--- internal/api/gen.go
+++
@@ -1,8 +0,0 @@
-// Package api contains code generated from api/openapi.yaml by oapi-codegen.
-// Do not edit the *.gen.go files by hand. Run `make openapi` to regenerate
-// after editing api/openapi.yaml.
-package api
-
-//go:generate oapi-codegen -config ../../api/cfg/types.yaml ../../api/openapi.yaml
-//go:generate oapi-codegen -config ../../api/cfg/server.yaml ../../api/openapi.yaml
-//go:generate oapi-codegen -config ../../api/cfg/client.yaml ../../api/openapi.yaml
--- internal/router/swagger_stub.go
+++
@@ -1,8 +0,0 @@
-//go:build !swagger
-
-package router
-
-import "github.com/go-chi/chi/v5"
-
-// mountSwagger is a no-op in builds without the "swagger" build tag.
-func mountSwagger(_ chi.Router, _ bool) {}
