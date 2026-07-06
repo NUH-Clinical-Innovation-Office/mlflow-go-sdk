4ac235ebec0d8901b84e63119cee5116c32c4551
Yong Cheng Low <lowyongcheng@hotmail.com>
2026-07-06 16:45:00 +0800

    chore: strip backend template scaffolding to clean library module

--- internal/router/swaggerui/embed.go
+++
@@ -1,9 +0,0 @@
-// Package swaggerui embeds the Swagger UI host page.
-package swaggerui
-
-import _ "embed"
-
-// IndexHTML is the Swagger UI host page served at /swagger.
-//
-//go:embed index.html
-var IndexHTML []byte
